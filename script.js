2666f7832de6e6fa0fd4fbcafb30b874e8f2bb94
Duong <duongnguyenthai78@gmail.com>
2023-01-26 14:18:24 -0500

    Graphical changes. Commited before separation.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import Note from "./note.js";
+import { ranInt, collide } from "./utils.js";
 
 var noteCount = 0;
 var noteManager = new Map();
@@ -20,7 +21,7 @@ function docMouseDown(e) {
         let selectedId = selected.id.slice(5);
         noteManager.get(parseInt(selectedId)).selected = true;
 
-        selected.style.backgroundColor = "red";
+        // selected.style.backgroundColor = "red";
         offX = e.clientX - selected.offsetLeft;
         offY = e.clientY - selected.offsetTop;
 
@@ -39,9 +40,11 @@ function docMouseDown(e) {
     }
     
     function cancelDrag(e){
-        selected.style.backgroundColor = "white";
+        // selected.style.backgroundColor = "none";
 
         updateDisplay(selected, false);
+
+        selected.style.zIndex = String(noteCount);
 
         document.onmouseup = null;
         document.onmousemove = null;
@@ -53,20 +56,25 @@ function docMouseDown(e) {
  * Handles the creation of new notes.
  */
 document.getElementById("newNoteBtn").onclick = function (e) {
+
+    //<p>Note ${noteCount}</p>
     
     let inject = `
-    <div class="note" id="note-${noteCount}">    
-        <p>Note ${noteCount}</p>
+    <div class="note" id="note-${noteCount}">
+        <p class="noteText" id="noteText-${noteCount}" style="display:none;">Lorem ipsum</p>
         <input type="text" class="noteInput" id="noteInput-${noteCount}" placeholder="Lorem ipsum">
         </input>
-        <p class="noteText" id="noteText-${noteCount}" style="display:none;">Lorem ipsum</p>
     </div>
     `
     document.getElementById("playground").innerHTML += inject;
 
     let newElem = document.getElementById("note-" + (noteCount));
-    newElem.style.top = String(newElem.offsetTop + e.clientY) + "px";
-    newElem.style.left = String(newElem.offsetLeft + e.clientX) + "px";
+    // newElem.style.top = String(newElem.offsetTop + e.clientY) + "px";
+    // newElem.style.left = String(newElem.offsetLeft + e.clientX) + "px";
+    newElem.style.left = ranInt(0, window.innerWidth - 150) + "px";
+    newElem.style.zIndex = String(noteCount);
+    newElem.style.backgroundColor = 
+        `rgb(${ranInt(100, 255)}, ${ranInt(100, 255)}, ${ranInt(100, 255)})`;
 
     let newNote = new Note(noteCount, false);
     noteManager.set(noteCount, newNote);
@@ -82,12 +90,12 @@ document.getElementById("newNoteBtn").onclick = function (e) {
             noteText.innerHTML = this.value;
             this.style.display = "none";
             noteText.style.display = "block";
+            display()
         }
     });
 
     $(".noteText").on("click",function toggleNoteInput(e) {
         display();
-        console.log("clicked");
         let noteId = this.id.slice(9);
         let noteInput = document.getElementById("noteInput-" + noteId);
         
@@ -103,10 +111,11 @@ document.getElementById("newNoteBtn").onclick = function (e) {
  * Handles the movement of notes.
  */
 function updateDisplay(selected, preview){
+    let noteHeight = selected.clientHeight * .9;
     let selectedId = selected.id.slice(5);
     let oldPos = noteList.indexOf(parseInt(selectedId));
     let newPos = Math.min(noteCount - 1, 
-        Math.floor(parseInt(selected.style.top.slice(0, -2)) / 102));
+        Math.floor(parseInt(selected.style.top.slice(0, -2)) / noteHeight));
     
     noteList.splice(oldPos, 1);
     noteList.splice(newPos, 0, parseInt(selectedId));
@@ -117,7 +126,9 @@ function updateDisplay(selected, preview){
  * Handles the display of notes.
 */
 function display(selectedId){
+    let curZ = 0;
     let curY = 0;
+    let noteHeight = 0;
 
     noteList.forEach((id) => {
         let noteText = document.getElementById("noteText-" + id);
@@ -129,19 +140,23 @@ function display(selectedId){
         }
 
         let elem = document.getElementById("note-" + id);
+        noteHeight = elem.clientHeight * .9;
         let originY = document.getElementById("playground").offsetTop;
         if (id != selectedId){
             // elem.style.top = String(originY + curY) + "px";
             $("#note-" + id).animate(
                 {top: String(originY + curY) + "px"}, { duration: 10, queue: false });
-            elem.style.left = "0px";
-            elem.style.zIndex = "0";
+            elem.style.left = String(Math.max(0, elem.offsetLeft)) + "px";
+            elem.style.zIndex = curZ;
         } else {
-            elem.style.zIndex = "1";
+            elem.style.zIndex = String(noteCount);
         }
-        curY += 152;
+        curY += noteHeight;
+        curZ += 1;
     });
 }
+
+
 
 //test
 
---
+++ utils.js
@@ -0,0 +1,25 @@
+export function ranInt(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+export function collide(el1, el2) {
+    if (el1 == null || el2 == null) return false;
+    
+    let rect1 = el1.getBoundingClientRect();
+    let rect2 = el2.getBoundingClientRect();
+
+    let collided = !(
+      rect1.top > rect2.bottom ||
+      rect1.right < rect2.left ||
+      rect1.bottom < rect2.top ||
+      rect1.left > rect2.right
+    );
+
+    // console.log("top: " + rect1.top + ", bottom: " + rect1.bottom);
+    // console.log("left: " + rect1.left + ", right: " + rect1.right);
+
+    // console.log("top: " + rect2.top + ", bottom: " + rect2.bottom);
+    // console.log("left: " + rect2.left + ", right: " + rect2.right);
+    console.log(collided)
+    return collided;
+}
